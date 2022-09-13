84a37be15cfa0b4f0e890df018b3a49208f07360
sneltyn <sneltyn@gmail.com>
2022-09-13 17:20:09 +0300

    Up

--- contracts/interfaces/IExchange.sol
+++ contracts/interfaces/IExchange.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.9;
 
 interface IExcange {
+
+     enum DEX {
+        UNISWAP,
+        ONE_INCH,
+        DE_SWAP
+    }
+
     struct SwapParams{
         uint256 type_;
+        DEX dex;
     }
 
     function swap(SwapParams memory params) external;
