2fbd6b8ea6414e37309901a670585925a1f9aad6
Novgorodskii Igor <novgorodskiji@gmail.com>
2022-09-24 15:15:15 +0300

    update address

--- app/contracts/addresses.ts
+++ app/contracts/addresses.ts
@@ -1,21 +1,21 @@
 const addresses: any = {
   factory: {
-    address: '0x5FC8e400ac068d0550ac1952C700660a32089982',
+    address: '0xc349fC3183856a1F21E8FD9FF5589eCbc77Af443',
   },
   deforex: {
-    address: '0x79057B7d5342486a3197401aF64b3189D33baf2d',
+    address: '0x7890F15ABE81E13B7a468f03a64704ed2D3fC802',
   },
   exchange: {
-    address: '0x3Ae730Cb337a971992459f3268978F59020e3A41',
+    address: '0x9cFC844fB66d7c58096cfe08dd6D02E4d29F6031',
   },
  uniswapExchange: {
-    address: '0xe8e9AD50ECCc956b7bC6f7fbC92C886035cf4c76',
+    address: '0x2E77B11923380cf2042b26f7ff2c4DcBFf7cf0bF',
   },
   AlpDaiUsdc: {
-    address: '0xf300a6ab5eea16bf09ca649c56da75e278062c8f',
+    address: '0x7b6462301229096c6ab8993ff0cf2c19ebc3adbd',
   },
   AlpDaiUsdt: {
-    address: '0xf300a6ab5eea16bf09ca649c56da75e278062c8f',
+    address: '0x7b6462301229096c6ab8993ff0cf2c19ebc3adbd',
   },
   USDT: { address: '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C' },
   USDC: { address: '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C' },
