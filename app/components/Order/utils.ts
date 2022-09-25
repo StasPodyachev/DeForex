c73ab0250b298149f294201a9150ebac76a9ae14
Novgorodskii Igor <novgorodskiji@gmail.com>
2022-09-25 14:45:52 +0300

    update contrract

--- app/components/Order/utils.ts
+++ app/components/Order/utils.ts
@@ -7,7 +7,7 @@ export const approve  = async (contractAprove, contract, MaxUint256 : any) => {
 
 export const approved  = async (contractAprove, addressWallet, contract : any) => {
   return await contractAprove?.allowance(contract, addressWallet).then((res) => {
-    // alert(res)
+    alert(res)
     return res._hex !== "0x00" ? true : false
   })
 }
