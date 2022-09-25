7661917fc7188344d2f252574683acfe9c32f32c
Novgorodskii Igor <novgorodskiji@gmail.com>
2022-09-25 12:52:00 +0300

    update wallet

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -5,10 +5,20 @@ import { publicProvider } from 'wagmi/providers/public';
 import { SessionProvider } from 'next-auth/react';
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../apolloClient'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 const { provider } = configureChains(defaultChains, [publicProvider()]);
 const client = createClient({
   provider,
   autoConnect: true,
+  new InjectedConnector({ chains }),
+  new WalletConnectConnector({
+    chains,
+    options: {
+      qrcode: true,
+    },
+  }),
+
 });
 
 function MyApp({ Component, pageProps }) {
